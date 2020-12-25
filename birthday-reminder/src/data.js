b02ccecb7e1866959c4cc7e19b63bd00ca6b3de7
PiyushChandra17 <47579287+PiyushChandra17@users.noreply.github.com>
2020-12-25 17:47:07 +0530

    Add files via upload

---
+++ birthday-reminder/src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+      id: 1,
+      name: 'Piyush Chandra',
+      age: 25,
+      image:
+        'https://scontent.fccu1-1.fna.fbcdn.net/v/t1.0-9/67154152_1355971584566245_4105705627911716864_n.jpg?_nc_cat=105&cb=846ca55b-311e05c7&ccb=2&_nc_sid=09cbfe&_nc_ohc=DZ4mFNCnrtoAX99bRN0&_nc_ht=scontent.fccu1-1.fna&oh=68a81a13371ad65fab93a77a2bbc4f13&oe=600D4BF0',
+    },
+    {
+      id: 2,
+      name: 'Hester Hogan',
+      age: 32,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+    },
+    {
+      id: 3,
+      name: 'Larry Little',
+      age: 36,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    },
+    {
+      id: 4,
+      name: 'Sean Walsh',
+      age: 34,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    },
+    {
+      id: 5,
+      name: 'Lola Gardner',
+      age: 29,
+      image:
+        'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    },
+  ];
+  
